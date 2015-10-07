25e3dceaf5864458a2132658e8b781292d7775ba
Juan A. Martinez <jamvius@gmail.com>
2015-10-07 22:57:32 +0200

    Add lesson 7

---
+++ sitepoint/nodejs-introduction/07-Querying-databases/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var http = require('http'),
+    path = require('path');
+
+var express = require('express');
+
+var logger = require('./logger'),
+    mappings = require('./data/mappings');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('redirector'));
+
+app.get('/', function (req, res) {
+  mappings.list(function (err, documents) {
+    res.render('index', {
+      mappings: documents
+    });
+  });
+});
+
+app.get('/:alias', function (req, res) {
+  mappings.get(req.params.alias, function (err, mapping) {
+    if (err) { return res.send(404); }
+    res.redirect(mapping);
+  });
+});
+
+http.createServer(app).listen(3000);
